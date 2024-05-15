380d66207175dfca27026e48452889d0e600619c
Gianni Castellano <gianniccastellano@gmail.com>
2024-05-15 17:27:17 -0400

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+function returnFirstTwoDrivers (drivers) {
+    const newArray = [];
+    newArray.push(drivers[0], drivers[1]);
+    return newArray;
+}
+
+function returnLastTwoDrivers (drivers) {
+    const newArray = [];
+    newArray.push(drivers[2], drivers[3]);
+    return newArray;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (fare) {
+    return function (multiplier) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers)
+}
